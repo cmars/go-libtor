57c7e4923e5aeb5635063574bd5087efed74f8fb
Casey Marshall <git@cmars.tech>
2022-02-25 18:55:10 -0600

    fix: sysdeps before testBuildMatrix

--- build/test_linux.go
+++ build/test_linux.go
@@ -9,6 +9,43 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
+func TestBuild() error {
+	var err error
+	err = TestBuildMatrix("sta", "sta", "sta")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("sta", "sta", "dyn")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("sta", "dyn", "sta")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("sta", "dyn", "dyn")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("dyn", "sta", "sta")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("dyn", "sta", "dyn")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("dyn", "dyn", "sta")
+	if err != nil {
+		return err
+	}
+	err = TestBuildMatrix("dyn", "dyn", "dyn")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func TestBuildMatrix(zlibType, opensslType, libeventType string) error {
 	var installPackages, staticTags []string
 
